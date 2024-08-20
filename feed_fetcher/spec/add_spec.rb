d0ba0feee6823a92b18d2dd0927b9e5b8c220250
Yana Mishkova-Kareva <yanamishkova@gmail.com>
2024-08-20 09:52:57 +0300

    Add unhappy path test for adding a feed

--- feed_fetcher/spec/add_spec.rb
+++ feed_fetcher/spec/add_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe "Feeds", type: :request do
         feed = JSON.parse(response.body)
         expect(feed["feed"]["url"]).to eq("http://rss.cnn.com/rss/cnn_topstories.rss")
     end
+
+    scenario "Sends a post request to add a feed with an invalid URL" do
+
+        post "http://localhost:3000/feeds/add", params: {url: ""}
+
+        expect(response).to have_http_status(500)
+        feed = JSON.parse(response.body)
+        expect(feed["error"]).to eq("Failed to create feed ")
+    end
 end
