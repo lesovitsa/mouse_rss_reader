968e263489561799a32b03cff1cbeb4a13d45130
Yana Mishkova-Kareva <yanamishkova@gmail.com>
2024-08-20 10:00:12 +0300

    Add unhappy path test for updating

--- feed_fetcher/spec/update_spec.rb
+++ feed_fetcher/spec/update_spec.rb
@@ -16,6 +16,13 @@ RSpec.describe "Feeds", type: :request do
         expect(result["url"]).to eq("http://rss.cnn.com/rss/cnn_topstories.rcs")
     end
 
+    scenario "Sends a post request to add a feed with an invalid URL" do
+
+        post "http://localhost:3000/feeds/update", params: {id: @feed["id"], url: ""}
+
+        expect(response).to have_http_status(500)
+    end
+
     after do
         @feed.destroy
     end
